209ed74e59b50bd954791fe90729c1a9a753cfb0
Nick Clark <n.clark@base2services.com>
2016-09-01 12:20:53 +1000

    use unrestricted powershell policy, fail on error

--- scripts/set_ec2_config.ps1
+++ scripts/set_ec2_config.ps1
@@ -1,3 +1,5 @@
+$ErrorActionPreference = "Stop"
+
 $EC2SettingsFile="C:\Program Files\Amazon\Ec2ConfigService\Settings\Config.xml"
 $xml = [xml](get-content $EC2SettingsFile)
 $xmlElement = $xml.get_DocumentElement()
